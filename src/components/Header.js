f1cfe11e2b1e3bfe2d42f0be46e1c384e0af07cb
Tony Tran <codemonkey713@gmail.com>
2021-04-15 19:45:15 -0500

    header component is complete with style css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import './Header.css';
 import { Col, Row } from 'react-bootstrap';
+import './Header.css';
 
 const Header = () => {
   return (
-    <>
-    <Row className='center-align flex-column'>
+    <Row className='center-align flex-column background'>
       <Col>
-        <h1>React Employee Directory</h1>
+        <div className='center-align text-light pt-2'>
+          <h2>React Employee Directory</h2>
+        </div>
+        <div className='center-align text-light pb-2'>
+          Click on carrots to filter by heading or use the search box to narrow your results.
+        </div>
       </Col>
     </Row>
-    </>
   );
 }
 
